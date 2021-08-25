f84fcddea729a82612b477893bac398480f5f09b
Eric Rovell <ericrovell@gmail.com>
2021-08-25 14:40:18 +0300

    create config files

---
+++ rollup.config.js
@@ -0,0 +1,56 @@
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+
+/**
+ * Generates Rollup Plugin Configuration.
+ */
+function getPluginsConfig(compilerOptions) {
+	return [
+		typescript({
+			tsconfigOverride: { compilerOptions }
+		}),
+		terser({
+			ecma: 2018,
+			module: true,
+			toplevel: true,
+			compress: { pure_getters: true },
+			format: { wrap_func_args: false }
+		})
+	];
+}
+
+/**
+ * Bundle everything into ESM module.
+ */
+export default [
+	/**
+   * Core bundle build in ESM
+   */
+	{
+		input: "src/index.ts",
+		output: {
+			file: "dist/index.mjs",
+			format: "es"
+		},
+		external: [
+			"svelte",
+			"svelte/store"
+		],
+		plugins: getPluginsConfig({ declaration: true })
+	},
+	/**
+   * Core bundle in CJS
+   */
+	{
+		input: "src/index.ts",
+		output: {
+			file: "dist/index.js",
+			format: "cjs",
+		},
+		external: [
+			"svelte",
+			"svelte/store"
+		],
+		plugins: getPluginsConfig({ declaration: false }),
+	}
+];
